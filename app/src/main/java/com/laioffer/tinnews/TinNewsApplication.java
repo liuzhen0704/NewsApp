864ac3b510625cedd346c0f405087d6a41e0d440
Ramsey <416034116@qq.com>
2020-11-01 17:43:46 -0400

    Deploy the environment, three buttion navigation and news api

---
+++ app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -0,0 +1,18 @@
+package com.laioffer.tinnews;
+
+import android.app.Application;
+
+import com.ashokvarma.gander.Gander;
+import com.ashokvarma.gander.imdb.GanderIMDB;
+import com.facebook.stetho.Stetho;
+
+public class TinNewsApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO: new code here.
+        Gander.setGanderStorage(GanderIMDB.getInstance());
+        Stetho.initializeWithDefaults(this);
+    }
+}
